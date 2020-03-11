e600afc5ad5ef532b5dc073c4e1d5266cc03883e
Ｍirko <zhouchao@ZC-MacBook-Pro.local>
2020-03-11 10:09:57 +0800

    add

---
+++ eureka-server/src/main/java/com/springcloudbase/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.springcloudbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mirko on 2020/3/2.
+ */
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ gateway/src/main/java/com/springcloudbase/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.springcloudbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mirko on 2020/3/10.
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
