96d7c50ace4d1f69784cd4a52946ecafb75e3a50
zhouc <515456737@qq.com>
2020-07-27 11:44:16 +0800

    对eurekaClient是否自动加载做自定义配置，参考com.springcloudbase.config.EurekaConfig.EurekaAutoConfig

--- user-api/src/main/java/com/springcloudbase/config/EurekaConfig.java
+++ user-api/src/main/java/com/springcloudbase/config/EurekaConfig.java
@@ -4,12 +4,12 @@ package com.springcloudbase.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
-@ConditionalOnProperty(prefix = "eureka.client.service-url", value = "defaultZone", matchIfMissing = true)
 @Slf4j
 public class EurekaConfig {
 
@@ -18,9 +18,21 @@ public class EurekaConfig {
       log.info(" {} 加载信息...",this.getClass().getName());
     }
 
-//    @EnableAutoConfiguration
-    public void EurekaConfig(){
 
+    /**
+     * 目的：如果配置了defaultZone就加载EurekaClientAutoConfiguration，如果没有配置就不加载
+     * 配置是否自动加载eureka客户端自动配置类
+     * matchIfMissing: 如果未设置属性是否匹配，默认为false(不通过，不加载)
+     * 这里设置为true，表示如果没有配置defaultZone将加载EurekaAutoConfig类
+     */
+    @Slf4j
+    @Configuration
+    @ConditionalOnProperty(prefix = "eureka.client.service-url", value = "defaultZone", matchIfMissing = true)
+    @EnableAutoConfiguration(exclude = {EurekaClientAutoConfiguration.class})
+    public static class EurekaAutoConfig{
+        public EurekaAutoConfig(){
+            log.warn("没有开启eureka..");
+        }
     }
 
 }
